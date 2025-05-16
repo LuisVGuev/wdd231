56ed0056f51aedeceb3bd30a0c144210640db597
mtrackingtechnologies <tag.interaction.web@gmail.com>
2025-05-15 23:15:07 -0300

    main

---
+++ scripts/navigation.js
@@ -0,0 +1,6 @@
+document.getElementById('menu-toggle').addEventListener('click', () => {
+  const nav = document.getElementById('main-nav');
+  nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
+});
+
+
